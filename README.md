4f5992513f1aa7a963f998178047bce9dd3eae70
Olivier Mantel <olivier.mantel@sage.com>
2021-07-15 18:04:58 +0200

    works

---
+++ README.md
@@ -0,0 +1,34 @@
+# GraphQL server using JSON WEB Token
+
+## Install
+```bash
+$ git clone https://github.com/oliviermantel/practice-x3-graphql-server.git
+$ cd practice-x3-graphql-server
+$ npm i 
+```
+
+## To start the Graphql server
+```bash
+$ npm start
+Running a GraphQL API server at localhost:4000/graphql
+```
+## Usage
+
+Open with a internet browser : http://localhost:4000/graphql
+
+### Graphql request
+
+Test with the Graphql request next :
+```graphql
+{token(clientId:"aa",secretOrPrivateKey:"hh",user:"kkk")}
+```
+
+### Graphql result
+
+```json
+{
+  "data": {
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhYSIsInN1YiI6ImtrayIsImF1ZCI6IiIsImlhdCI6MTYyNjI3NzkyNywiZXhwIjoxNjI2Mjc4NTI3fQ"
+  }
+}
+```
--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
- 
+
+const jwt=require('practice-x3-jwt');
+const lifetime=600
+const signAlgorithm = "HS256";
+const options = { algorithm: signAlgorithm}
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    token(clientId: String, secretOrPrivateKey: String,user:String ) :String
   }
 `);
  
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+  token: ({clientId, secretOrPrivateKey,user}) => {
+    const nowInSeconds = Math.floor(Date.now() / 1000) - 30;
+    const expInSeconds = nowInSeconds + (lifetime || 300);
+    const payload = {
+        iss: clientId,
+        sub: user,
+        aud: '',
+        iat: nowInSeconds,
+        exp: expInSeconds,
+      };
+    
+    let ret = jwt.getToken(payload,secretOrPrivateKey,options);
+    return ret;
+  }
 };
  
 var app = express();
